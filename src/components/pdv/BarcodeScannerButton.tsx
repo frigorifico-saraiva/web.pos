1a2fe1a6b816211bac2edf305c149ace8e0216c4
Hugo Belém <belem.hugo@hotmail.com>
2026-09-25 18:33:02 -0300

    chore: DUPLICATE_SCAN_COOLDOWN_MS 2500

--- src/components/pdv/BarcodeScannerButton.tsx
+++ src/components/pdv/BarcodeScannerButton.tsx
@@ -12,7 +12,7 @@ type Props = {
   onDetected: (code: string) => Promise<boolean> | boolean;
 };
 
-const DUPLICATE_SCAN_COOLDOWN_MS = 3000;
+const DUPLICATE_SCAN_COOLDOWN_MS = 2500;
 
 export function BarcodeScannerButton({ onDetected }: Props) {
   const [isActive, setIsActive] = useState(false);
